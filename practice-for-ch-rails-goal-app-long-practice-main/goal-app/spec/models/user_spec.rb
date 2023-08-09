f40cf1829d8cb94a665dc1df16164193ce8a02c1
Vincent <vshan0816@gmail.com>
2023-08-09 14:28:22 -0400

    Finish installation

--- practice-for-ch-rails-goal-app-long-practice-main/goal-app/spec/models/user_spec.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal-app/spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:incomplete_user){User.new(username:"", password:"password")}
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:session_token) }
+  it { should validate_presence_of(:password_digest) }
 end
