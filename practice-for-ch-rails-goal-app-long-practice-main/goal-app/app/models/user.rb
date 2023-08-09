bfe6ed699c7dd698937a236379c8733dcc2bd332
Dan Lee <danielhlee4@gmail.com>
2023-08-09 17:34:33 -0400

    add auth methods

--- practice-for-ch-rails-goal-app-long-practice-main/goal-app/app/models/user.rb
+++ practice-for-ch-rails-goal-app-long-practice-main/goal-app/app/models/user.rb
@@ -6,4 +6,33 @@ class User < ApplicationRecord
     attr_reader :password
 
     before_validation :ensure_session_token
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::urlsafe_base64
+    end
 end
